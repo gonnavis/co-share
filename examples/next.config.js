35093c0a2d6455075e03962f6bd8c384eb01e617
Bela Bohlender <bbohlender@coconut-capital.com>
2021-09-24 17:45:57 +0200

    add prism styles

--- examples/next.config.js
+++ examples/next.config.js
@@ -1,32 +1,32 @@
 const withPurgeCss = require("next-purgecss")
-const rehypePrism = require('@mapbox/rehype-prism')
-const withImages = require("next-images");
+const rehypePrism = require("@mapbox/rehype-prism")
+const withImages = require("next-images")
 
 const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [rehypePrism]
-  }
+    extension: /\.mdx?$/,
+    options: {
+        remarkPlugins: [],
+        rehypePlugins: [rehypePrism],
+    },
 })
 
 module.exports = withImages(
-  withMDX(
-    withPurgeCss({
-      images: {
-        loader: 'custom'
-      },
-      basePath: '/co-share',
-      assetPrefix: '/co-share',
-      eslint: {
-        ignoreDuringBuilds: true,
-      },
-      pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-      trailingSlash: true,
-      purgeCssPaths: ["pages/**/*", "components/**/*"],
-      purgeCss: {
-        whitelistPatterns: () => [/^.token/, /^code/, /^html$/, /^body$/],
-      },
-    })
-  )
+    withMDX(
+        withPurgeCss({
+            images: {
+                loader: "custom",
+            },
+            basePath: "/co-share",
+            assetPrefix: "/co-share",
+            eslint: {
+                ignoreDuringBuilds: true,
+            },
+            pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+            trailingSlash: true,
+            purgeCssPaths: ["pages/**/*", "components/**/*"],
+            purgeCss: {
+                safelist: ["body", "html"],
+            },
+        })
+    )
 )
